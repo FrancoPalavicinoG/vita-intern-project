ec1a30fe8827dca027e3e1074a8c7f68038c8414
Franco Palavicino <fppalavicino@miuandes.cl>
2025-11-17 11:37:32 -0300

    fix(db): appointments model

--- vita-backend/db/init.sql
+++ vita-backend/db/init.sql
@@ -9,23 +9,23 @@ CREATE TABLE clients (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255),
     phone VARCHAR(50),
-    plan_name VARCHAR(255) NOT NULL,
-    total_sessions INT NOT NULL,
-    used_sessions INT NOT NULL DEFAULT 0,
+    planName VARCHAR(255) NOT NULL,
+    totalSessions INT NOT NULL,
+    usedSessions INT NOT NULL DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE appointments (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    client_id INT NOT NULL,
+    clientId INT NOT NULL,
     date DATETIME NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (client_id) REFERENCES clients(id)
+    FOREIGN KEY (clientId) REFERENCES clients(id)
 );
 
 INSERT INTO clients 
-(name, email, phone, plan_name, total_sessions, used_sessions)
+(name, email, phone, planName, totalSessions, usedSessions)
 VALUES 
 ("Pepito Fry", "pepito@example.com", "+56911111111", "Plan 10 sesiones", 10, 2),
 ("Ana Pérez", "ana@example.com", "+56922222222", "Plan 5 sesiones", 5, 5);
